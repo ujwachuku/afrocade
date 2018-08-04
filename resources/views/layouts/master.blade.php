bed01be05b2f577e6122513fc1234287cd32bf9f
Uzoma Jaja-Wachuku <ujwachuku@gmail.com>
2018-08-04 13:03:16 +0300

    Added GA code snippet

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -93,6 +93,16 @@ function createsoundbite(sound){
 }
 
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123389558-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-123389558-1');
+</script>
+
 @yield('scripts')
 </body>
 </html>
