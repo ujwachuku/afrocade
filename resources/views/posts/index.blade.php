1b089b2dc983f65f79e03124ea1054b5b1aebd2f
Uzoma <ujwachuku@gmail.com>
2020-10-06 13:15:10 +0300

    Col md 4 + infinity scroll + added row for video chunk on post view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -60,22 +60,26 @@
                         <div class="row">
                             @if(count($articles) > 0)
                             <div class="infinite-scroll">
-                                @foreach($articles as $article)
-                                <div class="col-lg-3 col-sm-6 col-xs-12">
-                                    <div class="h-video row">
-                                        <div class="col-sm-12 col-xs-6">
-                                            <div class="v-img">
-                                                <a href="{{ route('articles.show', $article->slug) }}" class="hvr-bob" onMouseover="afroHover.playclip()" onclick="afroClick.playclip()">
-                                                    <img src="{{ asset(Voyager::image($article->thumbnail('cropped'))) }}" alt="{{ $article->title }}">
-                                                </a>
+                                @foreach($articles->chunk(4) as $articleChunk)
+                                <div class="row">
+                                    @foreach($articleChunk as $article)
+                                    <div class="col-lg-3 col-sm-6 col-xs-12">
+                                        <div class="h-video row">
+                                            <div class="col-sm-12 col-xs-6">
+                                                <div class="v-img">
+                                                    <a href="{{ route('articles.show', $article->slug) }}" class="hvr-bob" >
+                                                        <img src="{{ asset(Voyager::image($article->thumbnail('cropped'))) }}" alt="{{ $article->title }}">
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-10 col-xs-6">
-                                            <div class="v-desc">
-                                                <a href="{{ route('articles.show', $article->slug) }}" title="{{  $article->title }}">{{ substr($article->title, 0, 60) }}...</a>
+                                            <div class="col-sm-10 col-xs-6">
+                                                <div class="v-desc">
+                                                    <a href="{{ route('articles.show', $article->slug) }}" title="{{  $article->title }}">{{ substr($article->title, 0, 60) }}...</a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                    @endforeach
                                 </div>
                                 @endforeach
                                 {{ $articles->links() }}
